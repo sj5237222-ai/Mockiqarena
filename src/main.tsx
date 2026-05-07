1d5216bbe3b7cf0b91a54086ea19769db7ec713e
sj5237222-ai <sj5237222@gmail.com>
2026-05-08 01:13:06 +0530

    Refactor App component to include Navbar and Layout

--- src/main.tsx
+++ src/main.tsx
@@ -4,24 +4,34 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
+import Mocks from "./pages/Mocks";
 
-function App() {
+function Layout() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navbar />
+
+      <div className="max-w-6xl mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/mocks" element={<Mocks />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
   </React.StrictMode>
 );
